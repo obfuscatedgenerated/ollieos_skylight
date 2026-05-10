7a614c8af601a19f960cd4a4666c4f7f7a6161a3
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-05-11 00:51:35 +0100

    escalate in root manually to allow easier debug

--- src/root/index.ts
+++ src/root/index.ts
@@ -1,4 +1,5 @@
-import type {PrivilegedProgram} from "ollieos/types";
+import type {Program} from "ollieos/types";
+import {Kernel} from "ollieos/kernel";
 
 export default {
     name: "sl_root",
@@ -9,7 +10,19 @@ export default {
     compat: "2.0.0",
     main: async (data) => {
         // extract from data to make code less verbose
-        const { kernel, process } = data;
+        const { kernel: userspace_kernel, process, term } = data;
+        let kernel: Kernel;
+
+        if (!userspace_kernel.privileged) {
+            const maybe_kernel = await userspace_kernel.request_privilege("Launch Skylight shell. To prevent this appearing, install the Skylight service or default shell.");
+            if (!maybe_kernel) {
+                term.writeln("Privilege request denied. Skylight cannot start.");
+                return 1;
+            }
+            kernel = maybe_kernel;
+        } else {
+            kernel = userspace_kernel as unknown as Kernel;
+        }
 
         // invoke sl_taskbar with privilege
         const spawn_result = kernel.spawn("sl_taskbar", [], undefined, true);
@@ -20,4 +33,4 @@ export default {
         process.detach();
         return 0;
     }
-} as PrivilegedProgram;
+} as Program;
